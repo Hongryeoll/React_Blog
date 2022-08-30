fe3d154b85f562f2131d5c196ab6ef96f08f27a8
Hongryeoll <hong462804@gmail.com>
2022-08-30 18:12:37 +0900

    🎉 React_blog 시작

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Browser, BrowserRouter, Route, Routes } from "react-router-dom"
+import BlogDetail from "./pages/BlogDetail/BlogDeatail"
+import Home from "./pages/Home/Home"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Home/>}
+        />
+        <Route 
+          path="/blog/:id"
+          element={<BlogDetail/>} 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
