b7e2b761745f6228f3edd6c4fc174b5856c9d970
Hongryeoll <hong462804@gmail.com>
2022-08-30 20:25:06 +0900

    📝 asset,datanase 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Browser, BrowserRouter, Route, Routes } from "react-router-dom"
 import BlogDetail from "./pages/BlogDetail/BlogDeatail"
 import Home from "./pages/Home/Home"
+import data from "./database/data.json"
 
 function App() {
   return (
